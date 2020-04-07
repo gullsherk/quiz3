27c57ac8c760a802956b03b62158ce3220bcfa38
gullsherk <gullbhi1998@gmail.com>
2020-04-07 22:58:14 +0500

    Final version quiz 3
    The final version of Quiz 3

--- quiz3.go
+++ quiz3.go
@@ -1,7 +1,49 @@
 package main
 
-import "fmt"
+import (
+    "html/template"
+    "net/http"
+)
+
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/index.html"))
+    data := "I AM THE BEST "
+    tmpl.Execute(w, data)
+}
+
+func pakistan(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/pakistan.html"))
+    data := "I AM THE BEST "
+    tmpl.Execute(w, data)
+}
+
+func southkorea(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/southkorea.html"))
+    data := "I AM THE BEST "
+    tmpl.Execute(w, data)
+}
+
+func france(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/france.html"))
+    data := "I AM THE BEST "
+    tmpl.Execute(w, data)
+}
+
+func message(w http.ResponseWriter, r *http.Request) {
+    tmpl := template.Must(template.ParseFiles("templates/message.html"))
+    data := "I AM THE BEST "
+    tmpl.Execute(w, data)
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+    http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates"))))
+
+    http.HandleFunc("/", mainPage)
+    http.HandleFunc("/pakistan", pakistan)
+    http.HandleFunc("/southkorea", southkorea)
+    http.HandleFunc("/france", france)
+    http.HandleFunc("/message", message)
+    
+    http.ListenAndServe(":8080", nil)
 }
